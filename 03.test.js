ff279f19ae819b6f0ccbfe939304e985a4b7466e
vajsp <442026460@qq.com>
2018-04-16 07:51:39 +0800

    拼地图代码

--- 03.test.js
+++ 03.test.js
@@ -1,28 +1,5 @@
-let arr = [1, 23123132, 2, 22, 1, 3, 3];
+var str = '四川省';
 
+var newStr = str.replace(/('省'|'市')/,'');
 
-
-function bubble(arr) {
-    var array = [].concat(arr);
-    var a, b, status
-
-    for (let j = 0; j < array.length - 1; j++) {
-        status = true
-        for (let i = 0; i < array.length - 1 - j; i++) {
-            a = array[i]
-            b = array[i + 1]
-            if (a > b) {
-                status = false;
-                array[i] = b;
-                array[i + 1] = a;
-            }
-        }
-        if (status) {
-            return array
-        }
-    }
-
-    return array
-}
-
-console.log(bubble(arr))
+console.log(newStr);
--- mianmainmain/04.异步.js
+++ mianmainmain/04.异步.js
@@ -4,14 +4,14 @@
 //2.需要等待才需要异步
 
 
-    前端使用异步的场景
-        定时任务 setTimeout setInverval
-        网络请求： ajax 动态加载img
-        时间绑定
+    // 前端使用异步的场景
+    //     定时任务 setTimeout setInverval
+    //     网络请求： ajax 动态加载img
+    //     事件绑定
 
-    同步异步的区别是什么
+    // 同步异步的区别是什么
 
-        同步会阻塞代码执行，而异步不会
-        alert是同步，setTimeout是异步
+    //     同步会阻塞代码执行，而异步不会
+    //     alert是同步，setTimeout是异步
     
     
