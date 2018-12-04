a2c699db39e57e7f5819daba4221bd2e85ad6920
mamingqun <maxrocky@maxrocky.com>
2018-12-04 11:10:22 +0800

    css揭秘

--- 02.test.js
+++ 02.test.js
@@ -1,9 +1,14 @@
-for (var i = 0; i < 10; ++i) {
-    ((i) => {
-        setTimeout(() => {
-            console.log(i);
-        }, 0);
-    })(i);
+var obj = {
+    name: 'tom'
+}    
+
+var  fn1 = function(){
+    console.log(this)
+}
+var fn2 = () => {
+    console.log(this)
 }
 
-
+fn1()
+fn1.call(obj)
+fn2.call(obj)
