bdd410987ef835566786ae4d483dbafad64a9333
马明群_迈动互联 <mamingqun@bdideal.com>
2019-11-18 18:51:08 +0800

    ？？

--- 00.练习/02.test.js
+++ 00.练习/02.test.js
@@ -1,1 +1,15 @@
-console.log(global)
+let koa = require('koa')
+let app = new koa()
+
+app.use(async function (ctx,next) { 
+  console.log(1111);
+  next()
+
+  console.log(3333);
+})
+
+app.use(async function (ctx,next) { 
+  console.log(22222);
+})
+
+app.listen(8080)
