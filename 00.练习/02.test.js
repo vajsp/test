01d6291b43daa6db8476afe2ec56154deb6f61b0
ma <442026460@qq.com>
2020-10-22 11:56:48 +0800

    保存

--- 00.练习/02.test.js
+++ 00.练习/02.test.js
@@ -1,4 +1,12 @@
-var a = 14000;
-var b = a.toLocaleString('en-US');
+var obj = '1234567843234';
 
-console.log(b);
+// var reg = /\d{1,3}(?=(\d{3}+$))/g;
+var reg = /\d{1,2}(?=(\d{2})+$)/g;
+
+var obj1 = obj.match(reg);
+
+console.log(obj1);
+
+var asdf = obj.replace(reg, '$&,');
+
+console.log(asdf);
