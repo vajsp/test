59ad6441ead588fa07f464b41391213d75d47fb8
ma <442026460@qq.com>
2020-10-22 11:52:30 +0800

    保存

--- 05.ts/01-01.基础/01-05.静态类型的深度理解.ts
+++ 05.ts/01-01.基础/01-05.静态类型的深度理解.ts
@@ -1,9 +1,13 @@
 interface Point {
-  x: number;
-  y: number;
+  x: number
+  y: number
 }
 
 const point: Point = {
   x: 3,
-  y: 4
-};
+  y: 4,
+}
+
+// 变量声明由js的动态声明 转为ts的静态作用域
+// 静态类型变量具有 类型的方法，比如String类型拥有string类型的方法
+// 这个例子中point具有Point的所有属性和方法
